f72df0d4f826393edd7a434fcd5192e929667e33
TryNErr <bhbhbh2002-one@yahoo.com>
2025-11-08 09:15:20 +0000

    Simplify backend to just function with URL - remove complex data schema

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { myFirstFunction } from './my-first-function/resource';
-import { data } from './data/resource';
 
 export const backend = defineBackend({
-  myFirstFunction,
-  data
+  myFirstFunction
+});
+
+// Add function URL to make it accessible via HTTP
+backend.myFirstFunction.resources.lambda.addFunctionUrl({
+  authType: 'NONE'
 });
