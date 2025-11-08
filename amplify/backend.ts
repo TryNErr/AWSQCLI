a784913a218f221d0808da1d83fdb3bac9cba5ab
TryNErr <bhbhbh2002-one@yahoo.com>
2025-11-08 09:01:58 +0000

    Simplify Amplify Gen 2 configuration to fix Lambda deployment

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,22 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { myFirstFunction } from './my-first-function/resource';
-import { data } from './data/resource';
-import { FunctionUrlAuthType, HttpMethod } from 'aws-cdk-lib/aws-lambda';
-import { Duration } from 'aws-cdk-lib';
+import { myFirstFunction } from './my-first-function/resource.js';
+import { data } from './data/resource.js';
 
 export const backend = defineBackend({
   myFirstFunction,
   data
-});
-
-// Enable function URL for direct HTTP access
-backend.myFirstFunction.resources.lambda.addFunctionUrl({
-  authType: FunctionUrlAuthType.NONE,
-  cors: {
-    allowCredentials: false,
-    allowedHeaders: ['*'],
-    allowedMethods: [HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.OPTIONS],
-    allowedOrigins: ['*'],
-    maxAge: Duration.days(1)
-  }
 });
