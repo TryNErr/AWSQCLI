cc507dce8ee069c09483522e59263dbdc8bf5416
TryNErr <bhbhbh2002-one@yahoo.com>
2025-11-08 08:36:48 +0000

    Simplify Amplify configuration - remove complex API setup

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { myFirstFunction } from './my-first-function/resource';
 import { data } from './data/resource';
-import { api } from './api/resource';
 
 export const backend = defineBackend({
   myFirstFunction,
-  data,
-  api
+  data
 });
--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -2,7 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const myFirstFunction = defineFunction({
   name: "quizwiz-api",
-  entry: "./handler.ts",
-  runtime: 18,
-  timeout: 30
+  entry: "./handler.ts"
 });
